0abed0fe841022087435ed2b095662215c619c99
Exiam6 <zz4330@nyu.edu>
2025-02-20 16:22:22 -0500

    add html-flask pipeline

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/IoT_H2')
+def hello_H2():
+    return "Hello,IoT_H2"
+
+if __name__ == '__main__':
+    app.run()
