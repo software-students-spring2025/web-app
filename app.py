25bb31651a619b1dfe87a4ed4e4ab8ce5f67f85a
jenniferyuuu <jy4172@nyu.edu>
2025-02-24 15:38:09 -0500

    set up flask

---
+++ app.py
@@ -0,0 +1,35 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for
+from dotenv import load_dotenv, dotenv_values
+
+load_dotenv()  # load environment variables from .env file
+
+def create_app():
+    """
+    Create and configure the Flask application.
+    returns: app: the Flask application object
+    """
+
+    app = Flask(__name__)
+    # load flask config from env variables
+    config = dotenv_values()
+    app.config.from_mapping(config)
+
+    @app.route("/")
+    def hello_world():
+        """
+        testing home route 
+        """
+        return "<p>Hello, World!</p>"
+    
+    return app
+
+
+app = create_app()
+
+if __name__ == "__main__":
+    FLASK_PORT = os.getenv("FLASK_PORT", "5000")
+    FLASK_ENV = os.getenv("FLASK_ENV")
+    print(f"FLASK_ENV: {FLASK_ENV}, FLASK_PORT: {FLASK_PORT}")
+
+    app.run(port=FLASK_PORT)
