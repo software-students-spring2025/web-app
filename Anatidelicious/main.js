3d0700346c27201e594dd3edadad70983d458f46
s-m-wang <smw9999@nyu.edu>
2025-02-24 19:35:17 -0500

    Delete Anatidelicious/main.js

--- Anatidelicious/main.js
+++
@@ -1,7 +0,0 @@
-
-let defaultImage = "images/breads/bakery.jpg";
-let mixedArray = [defaultimage,"Menu Item Name Here", "Description", "$10.00", "100 Calories"];
-const add = document.getElementById('add');
-add.addEventListener('click', (event) => {
-
-})
