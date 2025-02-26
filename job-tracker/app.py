4cfb5edd5b69bd4df32ef3880ebf70608c4682b1
ivapark <cmpark1656@gmail.com>
2025-02-25 23:05:05 -0500

    Save local changes before merging

--- job-tracker/app.py
+++ job-tracker/app.py
@@ -48,6 +48,18 @@ def create_app():
     @app.route('/signup')
     def signup():
         return render_template("signup.html")
+    
+
+    @app.route('/addapplication')
+    def addapplication():
+        current_date = datetime.now().strftime("%B %d, %Y")
+        return render_template('addapplication.html', current_date=current_date)
+    
+    @app.route('/addnew')
+    def addnew():
+        return render_template('addnew.html')
+
+
 
     # home page
     @app.route('/home')
