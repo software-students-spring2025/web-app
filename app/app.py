4526d1d384db33e7aedc7f1c7d8129e0ec93dc8d
AnnaTheYe <azy2006@nyu.edu>
2025-02-25 22:34:07 -0500

    Changed implementation of cart in app.py to use Cart.py mongodb altering, changed files in app/models to snake_case, changed id to item_id

--- app/app.py
+++ app/app.py
@@ -1,29 +1,42 @@
 from flask import Flask, render_template as rt, session, redirect, url_for, request
 import pymongo
+from flask_pymongo import PyMongo
 app = Flask(__name__)
 app.secret_key = 'duck'
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client.my_database
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+mongo = PyMongo(app)
+
 menu_collection = db.menu_items
-menu_collection.drop()
+menu_collection.drop() # if we drop this, the cart resets every time, cart information not saved for customer
+carts = db.carts
+carts.drop()
 if menu_collection.count_documents({}) == 0:
     menu_collection.insert_many([
-        {'id': 'baguette', 'name': 'Baguette', 'description': 'A scrumptious baguette that would make France proud.', 'price': 1},
-        {'id': 'croissant', 'name': 'Croissant', 'description': 'Decadent and Flakey. Perfect piece of bread.', 'price': 2},
-        {'id': 'focaccia', 'name': 'Focaccia', 'description': 'Oven baked Italian flat bread. Very Tasty!.', 'price': 3},
-        {'id': 'sourdough', 'name': 'Sourdough', 'description': "Tangy and delicious loaf that you'll love", 'price': 4},
-        {'id': 'whole_grain', 'name': 'Whole Grain', 'description': "Tangy and delicious loaf that you'll love", 'price': 5},
-        {'id': 'almond_cookie', 'name': 'Almond Cookie', 'description': "Delicious cookie topped with Almonds",'price': 6},
-        {'id': 'chocolate_cookie', 'name': 'Chocolate Cookie', 'description': "Rich and flavorful cookie for your chocolate cravings",'price': 7},
-        {'id': 'chocolate_chip_cookie', 'name': 'Chocolate Chip Cookie', 'description': "Our take on the classic simple cookie!", 'price': 8},
-        {'id': 'chocolate_filled_cookie', 'name': 'Chocolate Filled Cookie', 'description': "Enjoy a crisp cookie and gooey center", 'price': 9},
-        {'id': 'gingerbread', 'name': 'Gingerbread', 'description': "Enjoy the seasonal cookie all year round!", 'price': 10},
-        {'id': 'matcha_cookie', 'name': 'Matcha Cookie', 'description': "Made with fresh green tea leaves at our bakery!", 'price': 11},
-        {'id': 'peanut_utter_cookie', 'name': 'Peanut Butter Cookie', 'description': "Soft and chewy while being sweet and salty", 'price': 12},
-        {'id': 'thumbprint_cookies', 'name': 'Thumbprint Cookies', 'description': "Shortbread Cookies with a delicious jam in the middle", 'price': 13},
+        {'item_id': 'baguette', 'name': 'Baguette', 'description': 'A scrumptious baguette that would make France proud.', 'price': 1},
+        {'item_id': 'croissant', 'name': 'Croissant', 'description': 'Decadent and Flakey. Perfect piece of bread.', 'price': 2},
+        {'item_id': 'focaccia', 'name': 'Focaccia', 'description': 'Oven baked Italian flat bread. Very Tasty!.', 'price': 3},
+        {'item_id': 'sourdough', 'name': 'Sourdough', 'description': "Tangy and delicious loaf that you'll love", 'price': 4},
+        {'item_id': 'whole_grain', 'name': 'Whole Grain', 'description': "Tangy and delicious loaf that you'll love", 'price': 5},
+        {'item_id': 'almond_cookie', 'name': 'Almond Cookie', 'description': "Delicious cookie topped with Almonds",'price': 6},
+        {'item_id': 'chocolate_cookie', 'name': 'Chocolate Cookie', 'description': "Rich and flavorful cookie for your chocolate cravings",'price': 7},
+        {'item_id': 'chocolate_chip_cookie', 'name': 'Chocolate Chip Cookie', 'description': "Our take on the classic simple cookie!", 'price': 8},
+        {'item_id': 'chocolate_filled_cookie', 'name': 'Chocolate Filled Cookie', 'description': "Enjoy a crisp cookie and gooey center", 'price': 9},
+        {'item_id': 'gingerbread', 'name': 'Gingerbread', 'description': "Enjoy the seasonal cookie all year round!", 'price': 10},
+        {'item_id': 'matcha_cookie', 'name': 'Matcha Cookie', 'description': "Made with fresh green tea leaves at our bakery!", 'price': 11},
+        {'item_id': 'peanut_utter_cookie', 'name': 'Peanut Butter Cookie', 'description': "Soft and chewy while being sweet and salty", 'price': 12},
+        {'item_id': 'thumbprint_cookies', 'name': 'Thumbprint Cookies', 'description': "Shortbread Cookies with a delicious jam in the middle", 'price': 13},
     ])
+
 menu_collection.create_index([("name", "text"), ("description", "text")])
+
+from models.Cart import Cart
+from models.MenuItem import MenuItem
 
 @app.route("/")
 def home():
@@ -51,7 +64,7 @@ def order():
 
 @app.route('/item_info/<item_id>')
 def item_info(item_id):
-    item = menu_collection.find_one({'id': item_id}, {"_id": 0})
+    item = menu_collection.find_one({'item_id': item_id})
     if not item:
         return 404
     return rt('item_info.html', item=item)
@@ -63,46 +76,69 @@ def initialize_cart():
 
 @app.route('/add_to_cart/<item_id>')
 def add_to_cart(item_id):
-    cart = session.get('cart', {})
-    cart[item_id] = cart.get(item_id, 0) + 1
-    session['cart'] = cart
+    customer_id = 100 # placeholder value
+
+    if not customer_id:
+        return redirect(url_for('login'))
+    Cart.add_to_cart(customer_id, item_id)
+
     return redirect(url_for('order'))
+
 
 @app.route('/cart')
 def cart():
-    cart = session.get('cart', {})
+    customer_id = 100 # placeholder value
+
+    if not customer_id:
+        return redirect(url_for('login'))
+
+    cart = Cart.get_cart(customer_id)
+
     cart_items = []
-    for item_id, quantity in cart.items():
-        item = menu_collection.find_one({'id': item_id}, {"_id": 0})
+    total_price = cart.get('total_price', 0)
+    total_items = 0
+
+    for item_id in set(cart.get('items')):
+        item = menu_collection.find_one({'item_id': item_id})
         if item:
-            item['quantity'] = quantity
-            cart_items.append(item)
-    total_items = sum(cart.values())
+            item_quantity = Cart.get_item_count(customer_id, item_id)
+            if item_quantity > 0:
+                total_items += item_quantity
+                item['quantity'] = item_quantity
+                cart_items.append(item)
+    print(cart_items)
     return rt('cart.html', cart_items=cart_items, total_items=total_items)
+
 
 @app.route('/remove_from_cart/<item_id>')
 def remove_from_cart(item_id):
-    cart = session.get('cart', {})
-    if item_id in cart:
-        if cart[item_id] > 1:
-            cart[item_id] -= 1
-        else:
-            del cart[item_id]
-        session['cart'] = cart
+    customer_id = 100 # placeholder value
+
+    if not customer_id:
+        return redirect(url_for('login'))
+    
+    cart = Cart.get_cart(customer_id)
+    
+    Cart.remove_from_cart(customer_id, item_id)
+
+    session['cart'] = cart
     return redirect(url_for('cart'))
 
 @app.route('/checkout')
 def checkout():
-    cart = session.get('cart', {})
+    customer_id = 100 # placeholder value
+
+    if not customer_id:
+        return redirect(url_for('login'))
+
+    cart = Cart.get_cart(customer_id)
     cart_items = []
-    total_price = 0
-    for item_id, quantity in cart.items():
-        item = menu_collection.find_one({'id': item_id}, {"_id": 0})
-        if item:
-            item['quantity'] = quantity
-            item_total = item['price'] * quantity
-            item['total'] = item_total
-            total_price += item_total
+    total_price = Cart.calculate_raw_total(customer_id)
+    for item_id in set(cart.get('items')):
+        item = menu_collection.find_one({'item_id': item_id})
+        if item and Cart.get_item_count(customer_id, item_id) > 0:
+            item['quantity'] = Cart.get_item_count(customer_id, item_id)
+            item['total'] = Cart.get_item_count(customer_id, item_id) * item.get('price')
             cart_items.append(item)
     return rt('checkout.html', cart_items=cart_items, total_price=total_price)
 
