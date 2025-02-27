577441369891a839e74d7322e17831684f2be3cd
peanutoil <harinib246@gmail.com>
2025-02-27 17:28:28 -0500

    route env setup stuff

---
+++ app.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+"""
+Example flask-based web application.
+See the README.md file for instructions how to set up and run the app in development mode.
+"""
+
+import os
+import datetime
+from flask import Flask, render_template, request, redirect, url_for
+import pymongo
+from bson.objectid import ObjectId
+from dotenv import load_dotenv, dotenv_values
+
+load_dotenv()  # load environment variables from .env file
+
+def create_app():
+    """
+    Create and configure the Flask application.
+    returns: app: the Flask application object
+    """
+
+    app = Flask(__name__)
+    # load flask config from env variables
+    config = dotenv_values()
+    app.config.from_mapping(config)
+
+    cxn = pymongo.MongoClient(os.getenv("MONGO_URI"))
+    db = cxn[os.getenv("MONGO_DBNAME")]
+
+    try:
+        cxn.admin.command("ping")
+        print(" *", "Connected to MongoDB!")
+    except Exception as e:
+        print(" * MongoDB connection error:", e)
+
+    @app.route("/",methods=["GET","POST"])
+    def home():
+        if request.method == "GET":
+            return render_template("index.html")
+        elif request.method == "POST":
+            print('hi')
+            return redirect("/test")
+
+
+    return app
+
+app = create_app()
+
+@app.route("/test")
+def test():
+    return render_template('test.html')
+
+if __name__ == "__main__":
+    FLASK_PORT = os.getenv("FLASK_PORT", "5000")
+    FLASK_ENV = os.getenv("FLASK_ENV")
+    print(f"FLASK_ENV: {FLASK_ENV}, FLASK_PORT: {FLASK_PORT}")
+
+    app.run(port=FLASK_PORT)
