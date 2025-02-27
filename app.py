5777487c9b6772321a8d622b91d7f1e683c091de
AndrewJung03 <andrewjung3245@gmail.com>
2025-02-26 22:53:18 -0500

    edited login.html + created homepage.html + route handling

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        return redirect(url_for('homepage'))
     return render_template("login.html")
+
+@app.route("/homepage")
+def homepage():
+    return render_template("homepage.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
