ed04737519de776fcc429212ab19d598b8973e6d
melissalkelly <melissalkelly1@gmail.com>
2025-02-27 15:29:48 -0500

    Update app.py
    Here is our base app.py file where we can add our functions.

--- app.py
+++ app.py
@@ -1,7 +1,58 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route("/")
-def hello():
-    return "Hello, World!"
+#!/usr/bin/env python3
+
+"""
+Pinkberries flask-based web application.
+
+"""
+
+import os
+import datetime
+from flask import Flask, render_template, request, redirect, url_for
+import pymongo
+from bson.objectid import ObjectId
+from dotenv import load_dotenv, dotenv_values ### You will need to install dotenv from terminal
+
+load_dotenv()  # load environment variables from .env file
+
+def create_app():
+    """
+    Create and configure the Flask application.
+    returns: app: the Flask application object
+    """
+
+    app = Flask(__name__)
+    # load flask config from env variables
+    config = dotenv_values()
+    app.config.from_mapping(config)
+
+    cxn = pymongo.MongoClient(os.getenv("MONGO_URI"))
+    db = cxn[os.getenv("MONGO_DBNAME")]
+
+    try:
+        cxn.admin.command("ping")
+        print(" *", "Connected to MongoDB!")
+    except Exception as e:
+        print(" * MongoDB connection error:", e)
+
+
+    ### Add your functions here: ###
+
+    @app.route("/")
+    def home():
+
+    @app.route("/create", methods=["POST"])
+    def create_post():
+
+
+
+
+### Here is where the app gets created: ###
+
+app = create_app()
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    FLASK_PORT = os.getenv("FLASK_PORT", "5000")
+    FLASK_ENV = os.getenv("FLASK_ENV")
+    print(f"FLASK_ENV: {FLASK_ENV}, FLASK_PORT: {FLASK_PORT}")
+
+    app.run(port=FLASK_PORT)
