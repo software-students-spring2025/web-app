7ea6df99810b7c93b010c48500a8107ff4f1ef56
jkm8294 <jkm8294@nyu.edu>
2025-02-27 14:35:34 -0500

    add edit editions

--- app.py
+++ app.py
@@ -46,25 +46,27 @@ def login():
 #add movie route - check to see if links correctly Jime 
 @app.route('/add_movie', methods=['GET', 'POST'])
 def add_movie():
-    if 'user_id' not in session:
-        return redirect(url_for('login'))
+   ## if 'user_id' not in session:
+       ## return redirect(url_for('login'))
     
     if request.method == 'POST':
         title = request.form['title']
         genre = request.form['genre']
-        release_year = request.form['year']
-        user_id = session['user_id']
+        release_year = request.form['release_year']
+        
         
         movies_collection.insert_one({
-            "user_id": user_id,
+
             "title": title,
             "genre": genre,
             "release_year": release_year
         })
-        flash('Movie added successfully!')
-        return redirect(url_for('dashboard'))
+        
+        flash('Movie added successfully!', 'success')
+        return redirect(url_for('home'))  # Redirect to home instead of dashboard
     
     return render_template('add.html')
+
 
 #edit movie route -- check to see if links correclty Jime
 @app.route('/edit_movie/<movie_id>', methods=['GET', 'POST'])
@@ -77,13 +79,14 @@ def edit_movie(movie_id):
     if request.method == 'POST':
         title = request.form['title']
         genre = request.form['genre']
-        release_year = request.form['year']
+        release_year = request.form['release_year']
         
         movies_collection.update_one(
             {"_id": ObjectId(movie_id)},
             {"$set": {"title": title, "genre": genre, "release_year": release_year}}
         )
-        flash('Movie updated successfully!')
+        
+        flash('Movie updated successfully!', 'success')
         return redirect(url_for('movie_details', movie_id=movie_id))
     
     return render_template('edit.html', movie=movie)
