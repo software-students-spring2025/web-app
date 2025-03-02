dfac6957627b24d564fca02c7702029acfe500bf
mam10023 <mam10023@nyu.edu>
2025-03-02 10:06:55 -0500

    edits

---
+++ users.py
@@ -0,0 +1,11 @@
+import pymongo
+from database import db
+from bson.objectid import ObjectId
+
+
+user = {
+    "name": "John Doe",
+    "email": "jd1234@nyu.edu",
+    "username": "jd1234",
+    "password": "password1"
+}
