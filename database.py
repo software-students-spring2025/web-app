52f3f682a9592bfbdf7fbf80b28c4f38a758afb6
mam10023 <mam10023@nyu.edu>
2025-03-02 10:58:42 -0500

    formatting

--- database.py
+++ database.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 # make a connection to the database server
 # do we leave it like this or hardcode it for our own?
 connection = pymongo.MongoClient("mongodb://your_username:your_username@your_host_name:27017") 
-
 db = connection["Forum"]    
 
 # with data from database
@@ -26,10 +25,6 @@ def post_detail(post_id):
     else:
         return "<h1>Post Not Found</h1>", 404
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
-
 
 @app.route("/create_post", methods = ['GET','POST'])
 def create_post():
@@ -44,21 +39,25 @@ def create_post():
         title = request.form.get("title")
         content = request.form.get("content")
 
-    new_post = {
-        "user": user,
-        "title": title,
-        "content": content,
-        "comment": [] # new post will have no comments
-    }
+        new_post = {
+            "user": user,
+            "title": title,
+            "content": content,
+            "comment": [] # new post will have no comments
+        }
 
-    result = db.posts.insert_one(new_post) # add post to database
+        result = db.posts.insert_one(new_post) # add post to database
 
-    if result.inserted_id:
-        # after making a post, where should user go?
-        redirect(url_for(index2)) # this can change
-    else:
-        # check error code?
-        return "<h1>Failed to add post</h1>", 404
+        if result.inserted_id:
+            # after making a post, where should user go?
+            return redirect(url_for('index')) # this can change
+        else:
+            # check error code?
+            return "<h1>Failed to add post</h1>", 404
     
     return render_template("create_post.html") # this actually shows the form to user i think
 
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
