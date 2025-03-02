f3cee4dd2c7b1402f9093eb65dd64d0b7af0c61e
eth3r3aI <adam.caojz@gmail.com>
2025-03-02 12:21:21 -0500

    Deleted Test Arguments for setting up

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import pymongo
-from bson.objectid import ObjectId
-import datetime
-
-from dotenv import load_dotenv
 import os
 
 from flask import Flask
@@ -22,43 +17,3 @@ def home():
     '''
 if __name__ == '__main__':
     app.run(debug=True)
-
-##########################################################
-# following is the original code
-# can be deleted after finishing set up
-
-# load_dotenv()
-
-# MONGO_DBNAME = os.getenv('MONGO_DBNAME')
-# MONGO_URI = os.getenv('MONGO_URI')
-# client = pymongo.MongoClient(MONGO_URI)
-
-# # Debuggings
-# if not MONGO_URI:
-#     raise ValueError("MONGO_URI is missing! Check your .env file.")
-
-# db = client["test-database"]
-
-# collection = db.test_collection
-
-# # doc = {
-# #     "name": "Foo Barstein",
-# #     "email": "fb1258@nyu.edu",
-# #     "message": "We loved with a love that was more than love.\n -Edgar Allen Poe",
-# #     "created_at": datetime.datetime.utcnow() # the date time now
-# # }
-
-# # mongoid = collection.insert_one(doc)
-
-# docs = collection.find({
-#     "email": "fb1258@nyu.edu"
-# })
-# for doc in docs:
-#     # print(doc)
-#     output = '{name} ({email}) said "{message}" at {date}.'.format(
-#         name = doc['name'],
-#         email = doc['email'],
-#         message = doc['message'],
-#         date = doc['created_at'].strftime("%H:%M on %d %B %Y") # nicely-formatted datetime
-#     )
-#     print(output)
