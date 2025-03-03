6b0db08b4d2be305e7515e1c9cf66f45194fdc61
Shuyuan Yang <>
2025-03-02 22:39:44 -0500

    update user_management_routes.py

--- Back-End/routes/user_management_routes.py
+++ Back-End/routes/user_management_routes.py
@@ -11,6 +11,9 @@ user_management_bp = Blueprint("user_management", __name__)
 def admin_dashboard():
     current_user = get_jwt_identity()
     admin_user = UserInformation.objects(username=current_user).first()
+    if not admin_user or admin_user.usertype != 1:
+        return redirect(url_for("message.get_message", message="Access denied. Admin required.", redirect=url_for("login.login")))
+    
     return render_template("admin-dashboard.html", admin=admin_user)
 
 # get all users list
@@ -40,21 +43,33 @@ def get_all_users():
             "avatar": user.avatar
         })
 
-    return render_template("userlist.html", users=user_list)
+    return render_template("userlist.html", usersInfo=user_list)
 
 
 # detailed information of a single user
-@user_management_bp.route('/<user_id>', methods=['GET'])
+@user_management_bp.route('/profile/<user_name>', methods=['GET'])
 @jwt_required()
-def get_user(user_id):
+def get_user(user_name):
+    current_user = get_jwt_identity()
+    logged_in_user = UserInformation.objects(username=current_user).first()
+    
+    # special case for the hardcoded profile link in base.html
+    if user_name == '67b8d6f05da61bc65fceb190':
+        # redirect
+        return redirect(url_for('user_management.get_user', user_name=current_user))
+    
     try:
-        user = UserInformation.objects(id=ObjectId(user_id)).exclude('password').first()
+        user = UserInformation.objects(username=user_name).exclude('password').first()
     except:
-        return jsonify({"error": "Invalid user ID format"}), 400
+        return redirect(url_for("message.get_message", message="Invalid user format", redirect=url_for("house.get_all_houses")))
 
     if not user:
-        return jsonify({"error": "User not found"}), 404
-
+        return redirect(url_for("message.get_message", message="User not found", redirect=url_for("house.get_all_houses")))
+    
+    # if the current user is admin or looking at their own profile
+    if logged_in_user.usertype != 1 and current_user != user_name:
+        return redirect(url_for("message.get_message", message="Permission denied: You can only view your own profile", redirect=url_for("house.get_all_houses")))
+    
     user_data = {
         "id": str(user.id),
         "username": user.username,
@@ -62,76 +77,66 @@ def get_user(user_id):
         "usertype": user.usertype,
         "avatar": user.avatar
     }
+    
+    # get user's wishlist items if guest
+    wishlist_data = []
+    if user.usertype == 0:
+        wishlist_items = Wishlist.objects(user=user)
+        
+        for item in wishlist_items:
+            house = item.house
+            wishlist_data.append({
+                "building": house.building,
+                "apt_num": house.apt_num,
+                "price": house.price,
+                "bedroom": house.bedroom,
+                "bathroom": house.bathroom,
+                "area": house.area,
+                "available_date": house.available_date.strftime("%Y-%m-%d") if house.available_date else None,
+                "city": house.address.split(',', 1)[1].strip() if ',' in house.address else house.address,
+                "picture": house.picture
+            })
 
-    return jsonify(user_data), 200
+    return render_template("profile.html", userInfo=user_data, houseInfo=wishlist_data)
 
 
 # delete user
-@user_management_bp.route('/delete/<user_id>', methods=['DELETE'])
+@user_management_bp.route('/delete/<user_name>', methods=['POST'])
 @jwt_required()
-def delete_user(user_id):
+def delete_user(user_name):
     # verify current user identity
     current_user = get_jwt_identity()
     admin_user = UserInformation.objects(username=current_user).first()
 
     if not admin_user:
-        return jsonify({"error": "User not found"}), 404
-
+        return redirect(url_for("message.get_message", message="User not found", redirect=url_for("login.login")))
+    
     if admin_user.usertype != 1:
-        return jsonify({"error": "Permission denied: Only admins can delete users"}), 403
+        return redirect(url_for("message.get_message", message="Permission denied: Only admins can delete users", redirect=url_for("house.get_all_houses")))
 
     # get the user to delete
     try:
-        user = UserInformation.objects(id=ObjectId(user_id)).first()
+        user = UserInformation.objects(username=user_name).first()
     except:
-        return jsonify({"error": "Invalid user ID format"}), 400
-
+        return redirect(url_for("message.get_message", message="Invalid user format", redirect=url_for("user_management.get_all_users")))
     if not user:
-        return jsonify({"error": "User to delete not found"}), 404
+        return redirect(url_for("message.get_message", message="User to delete not found", redirect=url_for("user_management.get_all_users")))
 
     # admin should not delete their own account
-    if str(user.id) == str(admin_user.id):
-        return jsonify({"error": "You cannot delete your own admin account"}), 403
+    if user.username == admin_user.username:
+        return redirect(url_for("message.get_message", message="You cannot delete your own admin account", redirect=url_for("user_management.get_all_users")))
 
-    # delete wishlist
+    # delete all wishlists associated with this user
     Wishlist.objects(user=user).delete()
 
     # delete user
     user.delete()
 
-    return jsonify({"message": "User deleted successfully"}), 200
+    return render_template("delete-user.html", 
+                      q_message="Are you sure that you want to delete this user?",
+                      username=user_name,
+                      email=user.email)
 
-
-
-# get user's wishlist
-@user_management_bp.route('/wishlist', methods=['GET'])
-@jwt_required()
-def get_wishlist():
-    current_user = get_jwt_identity()
-    user = UserInformation.objects(username=current_user).first()
-
-    if not user:
-        return jsonify({"error": "User not found"}), 404
-
-    wishlist_items = Wishlist.objects(user=user)
-    
-    wishlist_data = []
-    for item in wishlist_items:
-        house = item.house
-        wishlist_data.append({
-            "wishlist_id": str(item.id),
-            "house_id": str(house.id),
-            "building": house.building,
-            "price": house.price,
-            "bedroom": house.bedroom,
-            "bathroom": house.bathroom,
-            "area": house.area,
-            "available_date": house.available_date.strftime("%Y-%m-%d") if house.available_date else None,
-            "address": house.address,
-            "picture": house.picture
-        })
-
-    return jsonify(wishlist_data), 200
 
 
 # Add house to wishlist, directly come with url
