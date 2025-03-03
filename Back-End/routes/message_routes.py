8219ebacda0f48cd6a095d81ee590757eb653141
Shuyuan Yang <>
2025-03-02 22:38:43 -0500

    update message_routes.py

--- Back-End/routes/message_routes.py
+++ Back-End/routes/message_routes.py
@@ -6,9 +6,21 @@ from bson import ObjectId
 message_bp = Blueprint("message", __name__)
 
 # get all buildings list
-@message_bp.route('/message', methods=['GET'])
+from flask import Blueprint, request, jsonify, render_template, redirect, url_for
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from models import Building, UserInformation, House
+from bson import ObjectId
+
+message_bp = Blueprint("message", __name__)
+
+# Updated to handle both GET and POST methods
+@message_bp.route('/message', methods=['GET', 'POST'])
 def get_message():
-    message = request.args.get("message", "No message provided")  
-    redirectWebsite = request.args.get("redirect", "/")  
-    #print(f"DEBUG: Message page accessed with message='{message}' and redirect='{redirectWebsite}'") 
+    if request.method == 'POST':
+        message = request.form.get("message", "No message provided")  
+        redirectWebsite = request.form.get("redirect", "/")
+    else:
+        message = request.args.get("message", "No message provided")  
+        redirectWebsite = request.args.get("redirect", "/")  
+        
     return render_template("message.html", message=message, redirectWebsite=redirectWebsite)
